c2d061d67a18e9daf37b7d3e118a33516f3b877b
João Lucas <jolucascosta05@gmail.com>
2025-10-15 18:44:13 -0300

    Adição do estado - no autômato

--- parte-1.py
+++ parte-1.py
@@ -3,17 +3,17 @@ Objetivo: Converter um AFND (com movimentos vazios) em um AFD
 Alfabeto: {0,1}
 Entrada: um arquivo com a tabela do AFND.
 Formato do arquivo de entrada:
-    Linha 0: a sequência de estados separados por espaço. EX: A B C D E F
+    Linha 0: a sequencia de estados separados por espaco. EX: A B C D E F
     Linha 1: estado inicial
-    Linha 2: estados finais separados por espaço (se houver mais de um estado final)
-    Linha 3 em diante: estado atual, espaço, caractere lido, espaço, próximo estado
-    Obs: respresentar a transição vazia por h.
+    Linha 2: estados finais separados por espaco (se houver mais de um estado final)
+    Linha 3 em diante: estado atual, espaco, caractere lido, espaco, proximo estado
+    Obs: respresentar a transicao vazia por h.
 
 Saída: um arquivo com a tabela do AFD
-Formato do arquivo de saída: o mesmo do arquivo de entrada.
+Formato do arquivo de saida: o mesmo do arquivo de entrada.
 
-Usar o GraphViz para fazer a exibição do grafo do AFND e do AFD.
-Usar o JFLAP e desenhar os dois autômatos: o de entrada e o de saída.
+Usar o GraphViz para fazer a exibicao do grafo do AFND e do AFD.
+Usar o JFLAP e desenhar os dois automatos: o de entrada e o de saida.
 """
 from graphviz import Digraph    
 
@@ -23,9 +23,9 @@ def formatar_como_tabela(dados):
     Formata os dados em formato de tabela
     """
     print("\n" + "=" * 60)
-    print("TABELA DE TRANSIÇÕES")
+    print("TABELA DE TRANSICOES".center(60))
     print("=" * 60)
-    print(f"{'Estado':<10} {'Transição 0':<15} {'Transição 1':<15}")
+    print(f"{'Estado':<10} {'Transicao p/ 0':<15} {'Transicao p/ 1':<15}".center(60))
     print("-" * 60)
     
     for item in dados:
@@ -40,7 +40,7 @@ def formatar_como_tabela(dados):
 
 # Ler arquivo e inserir linhas numa lista
 linhas_arquivo_AFND = []
-with open("entrada.txt", "r") as f:
+with open("entrada_AFND.txt", "r") as f:
     for linha in f:
         linhas_arquivo_AFND.append(linha)
 
@@ -73,65 +73,68 @@ i = 0
 while i < len(transicoes_AFD):
     estado_quebrado = list(transicoes_AFD[i][0])
     
-    for j in estado_quebrado:
-        for k in range(tam_transicoes_AFND):
-            # movimentos p/ 0 com h antes
-            if transicoes_AFND[k][0] in estado_quebrado and "h" in transicoes_AFND[k]:
-                estado_atual = transicoes_AFND[k][4]
-                l = 0
-                while l < tam_transicoes_AFND:
-                    if estado_atual in transicoes_AFND[l][0] and "h" in transicoes_AFND[l]:
-                        estado_atual = transicoes_AFND[l][4]
-                    if estado_atual in transicoes_AFND[l][0] and "0" in transicoes_AFND[l]:
-                        transicoes_AFD[i][1][0] += transicoes_AFND[l][4]
-                        transicoes_AFD[i][1][0] = "".join(sorted(set(transicoes_AFD[i][1][0])))
-                    l += 1
-            
-            # movimentos p/ 0 com h depois
-            if transicoes_AFND[k][0] in estado_quebrado and "0" in transicoes_AFND[k]:
-                if transicoes_AFND[k][4] in transicoes_AFD[i][1][0]:
-                    continue
-                transicoes_AFD[i][1][0] += transicoes_AFND[k][4]
-
-                estado_atual = transicoes_AFND[k][4]
-                l = 0
-                while l < tam_transicoes_AFND:
-                    if estado_atual in transicoes_AFND[l][0] and "h" in transicoes_AFND[l]:
-                        estado_atual = transicoes_AFND[l][4]
-                        transicoes_AFD[i][1][0] += transicoes_AFND[l][4]
-                        transicoes_AFD[i][1][0] = "".join(sorted(set(transicoes_AFD[i][1][0])))
-                    l += 1
-
-            # movimentos p/ 1 com h antes
-            if transicoes_AFND[k][0] in estado_quebrado and "h" in transicoes_AFND[k]:
-                estado_atual = transicoes_AFND[k][4]
-                l = 0
-                while l < tam_transicoes_AFND:
-                    if estado_atual in transicoes_AFND[l][0] and "h" in transicoes_AFND[l]:
-                        estado_atual = transicoes_AFND[l][4]
-                    if estado_atual in transicoes_AFND[l][0] and "1" in transicoes_AFND[l]:
-                        transicoes_AFD[i][1][1] += transicoes_AFND[l][4]
-                        transicoes_AFD[i][1][1] = "".join(sorted(set(transicoes_AFD[i][1][1])))
-                    l += 1
-
-            # movimentos p/ 1 com h depois
-            if transicoes_AFND[k][0] in estado_quebrado and "1" in transicoes_AFND[k]:
-                if transicoes_AFND[k][4] in transicoes_AFD[i][1][1]:
-                    continue
-                transicoes_AFD[i][1][1] += transicoes_AFND[k][4]
-
-                estado_atual = transicoes_AFND[k][4]
-                l = 0
-                while l < tam_transicoes_AFND:
-                    if estado_atual in transicoes_AFND[l][0] and "h" in transicoes_AFND[l]:
-                        estado_atual = transicoes_AFND[l][4]
-                        transicoes_AFD[i][1][1] += transicoes_AFND[l][4]
-                        transicoes_AFD[i][1][1] = "".join(sorted(set(transicoes_AFD[i][1][1])))
-                    l += 1
+    for k in range(tam_transicoes_AFND):
+        # movimentos p/ 0 com h antes
+        if transicoes_AFND[k][0] in estado_quebrado and "h" in transicoes_AFND[k]:
+            estado_atual = transicoes_AFND[k][4]
+            l = 0
+            while l < tam_transicoes_AFND:
+                if estado_atual in transicoes_AFND[l][0] and "h" in transicoes_AFND[l]:
+                    estado_atual = transicoes_AFND[l][4]
+                if estado_atual in transicoes_AFND[l][0] and "0" in transicoes_AFND[l]:
+                    transicoes_AFD[i][1][0] += transicoes_AFND[l][4]
+                    transicoes_AFD[i][1][0] = "".join(sorted(set(transicoes_AFD[i][1][0])))
+                l += 1
         
-        # colocar estados em ordem alfabetica
-        transicoes_AFD[i][1][0] = "".join(sorted(transicoes_AFD[i][1][0]))
-        transicoes_AFD[i][1][1] = "".join(sorted(transicoes_AFD[i][1][1]))
+        # movimentos p/ 0 com h depois
+        if transicoes_AFND[k][0] in estado_quebrado and "0" in transicoes_AFND[k]:
+            if transicoes_AFND[k][4] in transicoes_AFD[i][1][0]:
+                continue
+            transicoes_AFD[i][1][0] += transicoes_AFND[k][4]
+
+            estado_atual = transicoes_AFND[k][4]
+            l = 0
+            while l < tam_transicoes_AFND:
+                if estado_atual in transicoes_AFND[l][0] and "h" in transicoes_AFND[l]:
+                    estado_atual = transicoes_AFND[l][4]
+                    transicoes_AFD[i][1][0] += transicoes_AFND[l][4]
+                    transicoes_AFD[i][1][0] = "".join(sorted(set(transicoes_AFD[i][1][0])))
+                l += 1
+
+        # movimentos p/ 1 com h antes
+        if transicoes_AFND[k][0] in estado_quebrado and "h" in transicoes_AFND[k]:
+            estado_atual = transicoes_AFND[k][4]
+            l = 0
+            while l < tam_transicoes_AFND:
+                if estado_atual in transicoes_AFND[l][0] and "h" in transicoes_AFND[l]:
+                    estado_atual = transicoes_AFND[l][4]
+                if estado_atual in transicoes_AFND[l][0] and "1" in transicoes_AFND[l]:
+                    transicoes_AFD[i][1][1] += transicoes_AFND[l][4]
+                    transicoes_AFD[i][1][1] = "".join(sorted(set(transicoes_AFD[i][1][1])))
+                l += 1
+
+        # movimentos p/ 1 com h depois
+        if transicoes_AFND[k][0] in estado_quebrado and "1" in transicoes_AFND[k]:
+            if transicoes_AFND[k][4] in transicoes_AFD[i][1][1]:
+                continue
+            transicoes_AFD[i][1][1] += transicoes_AFND[k][4]
+
+            estado_atual = transicoes_AFND[k][4]
+            l = 0
+            while l < tam_transicoes_AFND:
+                if estado_atual in transicoes_AFND[l][0] and "h" in transicoes_AFND[l]:
+                    estado_atual = transicoes_AFND[l][4]
+                    transicoes_AFD[i][1][1] += transicoes_AFND[l][4]
+                    transicoes_AFD[i][1][1] = "".join(sorted(set(transicoes_AFD[i][1][1])))
+                l += 1
+    
+    # colocar estados em ordem alfabetica
+    # transicoes_AFD[i][1][0] = "".join(sorted(transicoes_AFD[i][1][0])) if transicoes_AFD[i][1][0] != "" else "-"
+    # transicoes_AFD[i][1][1] = "".join(sorted(transicoes_AFD[i][1][1])) if transicoes_AFD[i][1][1] != "" else "-"
+    for j in range(2):  
+        valor = transicoes_AFD[i][1][j]
+        transicoes_AFD[i][1][j] = "".join(sorted(valor)) if valor != "" else "-"
+
 
     cont = 0
     while cont < len(transicoes_AFD):
@@ -143,10 +146,10 @@ while i < len(transicoes_AFD):
             # Ignorar valores vazios ou None
             if conexao == "" or conexao is None:
                 continue
-            # Verifica se o elemento já existe em alguma lista[i][0]
+            # Verifica se o elemento ja existe em alguma lista[i][0]
             existe = any(conexao == item[0] for item in transicoes_AFD)
 
-            # Se não existe, adiciona um novo item com sublista vazia
+            # Se nao existe, adiciona um novo item com sublista vazia
             if not existe:
                 transicoes_AFD.append([conexao, ["", ""]])
 
@@ -161,7 +164,7 @@ formatar_como_tabela(transicoes_AFD)
 print("Estados Finais:", estados_finais)
 
 # criar arquivo de saida do AFD
-with open("saida_AFD.txt", "w") as f:
+with open("saidas/saida_AFD.txt", "w") as f:
     # Linha com os estados do automato
     for item in transicoes_AFD:
         f.write(item[0] + " ")
